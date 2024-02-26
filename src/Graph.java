8d7513ef2437489fa2feda1eceed1ba3b1495a3e
Gabriel Paquet <gabriel.paquet@student.vinci.be>
2024-02-26 11:21:39 +0100

    graph init

---
+++ src/Graph.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class Graph {
+
+  public Graph (File cities, File roads){
+
+  }
+
+  public void calculerItineraireMinimisantNombreRoutes(String ville1, String ville2){
+
+  }
+
+  public void calculerItineraireMinimisantKm(String ville1, String ville2){
+
+  }
+
+}
