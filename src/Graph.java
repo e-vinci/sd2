f16ea906ba06f165da2fe0ce4f07a702a101c0cc
Gabriel Paquet <gabriel.paquet@student.vinci.be>
2024-03-13 09:00:09 +0100

    mMethode des routes finie

--- src/Graph.java
+++ src/Graph.java
@@ -2,9 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -59,21 +57,55 @@ public class Graph {
     } catch (IOException e) {
       e.printStackTrace();
     }
-    System.out.println(this.roads);
   }
 
-  public void calculerItineraireMinimisantNombreRoutes(String source, String destination){
-    // UNE FILE ET UN ENSEMBLE DES SOMMETS DEJA VISITES
-    Queue<Integer> aVisiter = new LinkedList<>();
+  public void calculerItineraireMinimisantNombreRoutes(String source, String destination) {
+    Queue<Integer> file = new LinkedList<>();
     Set<Integer> villesVisitees = new HashSet<>();
+    Map<Integer, Integer> origine = new HashMap<>();
 
-    villesVisitees.add(citiesToInt.get(source));
     int villeActuelle = citiesToInt.get(source);
     boolean arrive = false;
-    int nombreRoutes = 0;
-    while(!arrive){
-      //aVisiter.addAll(roadsSourceDestination.get())
+
+    while (!arrive) {
+      for (int i : roads.get(villeActuelle)) {
+        if (!file.contains(i) && !villesVisitees.contains(i)) {
+          file.add(i);
+          origine.put(i, villeActuelle);
+        }
+      }
+      int villeSuivante = file.poll();
+      villesVisitees.add(villeSuivante);
+
+      if (!intToCities.get(villeSuivante).equals(destination))
+        villeActuelle = villeSuivante;
+      else
+        arrive = true;
     }
+
+    int to = citiesToInt.get(destination);
+    int from;
+    boolean revenu = false;
+    String s = "";
+    int nbRoutes = 0;
+    double nbKm = 0;
+
+    while(!revenu){
+      from = origine.get(to);
+      double distance = Util.distance(coordinates.get(from).get(0), coordinates.get(from).get(1),
+          coordinates.get(to).get(0), coordinates.get(to).get(1));
+
+      s = intToCities.get(from) + " -> " + intToCities.get(to) + " (" + String.format("%.2f", distance) + " km)\n" + s;
+
+      nbRoutes++;
+      nbKm += distance;
+
+      to = from;
+      if(from == citiesToInt.get(source))
+        revenu = true;
+    }
+    s = "Itinéraire de " + source + " à " + destination + ": " + nbRoutes + " routes et " + nbKm + " km\n" + s;
+    System.out.println(s);
   }
 
   public void calculerItineraireMinimisantKm(String source, String destination){
